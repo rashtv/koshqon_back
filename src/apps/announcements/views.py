10b142bad7fa47662c9fcc9a357d18d8c326aead
Ilyas Rashitov <rashtv.19@gmail.com>
2024-03-28 23:33:08 +0500

    feat:announcements_filtering

--- src/apps/announcements/views.py
+++ src/apps/announcements/views.py
@@ -1,3 +1,5 @@
+import base64
+
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status, permissions
 from rest_framework.decorators import permission_classes
@@ -19,7 +21,29 @@ class AnnouncementAPIView(APIView):
         responses={200: AnnouncementSerializer(many=True)},
     )
     def get(self, request):
+        query_params = request.query_params
         announcements = Announcement.objects.filter(is_deleted=False).order_by('-created_at')
+
+        district = query_params.get('district', None)
+        if district:
+            announcements = announcements.filter(district=district)
+
+        area_gt = query_params.get('area_gt', None)
+        if area_gt:
+            announcements = announcements.filter(area__gt=area_gt)
+
+        area_lt = query_params.get('area_lt', None)
+        if area_lt:
+            announcements = announcements.filter(area__lt=area_lt)
+
+        floor_location_gt = query_params.get('floor_location_gt', None)
+        if floor_location_gt:
+            announcements = announcements.filter(floor_location__gt=floor_location_gt)
+
+        floor_location_lt = query_params.get('floor_location_lt', None)
+        if floor_location_lt:
+            announcements = announcements.filter(floor_location__lt=floor_location_lt)
+
         serializer = AnnouncementSerializer(announcements, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -29,6 +53,24 @@ class AnnouncementAPIView(APIView):
         responses={201: AnnouncementSerializer()},
     )
     def post(self, request):
+        request.data['user'] = request.user.id
+
+        # images = request.data.get('images', None)
+        # announcement_data = request.data.copy()
+        # announcement_data['images'] = []
+        # if images:
+        #     for image in images:
+        #         try:
+        #             image_data = base64.b64decode(image)
+        #             announcement_data['images'].append(image_data)
+        #         except Exception as e:
+        #             return Response(
+        #                 {'error': 'Failed to decode image data.'},
+        #                 status=status.HTTP_400_BAD_REQUEST,
+        #             )
+
+        # serializer = AnnouncementSerializer(data=announcement_data)
+
         serializer = AnnouncementSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
