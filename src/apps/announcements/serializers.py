ba877e8cc94718a108799615903acfd25b059a07
Ilyas Rashitov <rashtv.19@gmail.com>
2024-04-04 01:31:13 +0500

    feat:announcement_validation

--- src/apps/announcements/serializers.py
+++ src/apps/announcements/serializers.py
@@ -1,3 +1,4 @@
+from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
 from apps.announcements.models import (
@@ -7,14 +8,27 @@ from apps.announcements.models import (
 
 
 class AnnouncementImageSerializer(serializers.ModelSerializer):
+    image = Base64ImageField()
 
     class Meta:
         model = AnnouncementImage
-        fields = ('id', 'image', )
+        fields = ('image', )
+
+    def save(self, announcement, *args, **kwargs):
+        image = AnnouncementImage.objects.create(announcement=announcement, image=self)
+        return image
 
 
 class AnnouncementSerializer(serializers.ModelSerializer):
-    images = AnnouncementImageSerializer(many=True, required=False)
+    images = AnnouncementImageSerializer(many=True, required=False, allow_null=True)
+
+    house_number = serializers.IntegerField(min_value=1, max_value=1024)
+    rooms_number = serializers.IntegerField(min_value=1, max_value=1024)
+    floors_number = serializers.IntegerField(min_value=1, max_value=1024)
+    floor_location = serializers.IntegerField(min_value=1, max_value=1024)
+    area = serializers.IntegerField(min_value=1, max_value=1024)
+    kitchen = serializers.IntegerField(min_value=1, max_value=1024)
+    bathroom = serializers.IntegerField(min_value=1, max_value=1024)
 
     class Meta:
         model = Announcement
@@ -22,10 +36,10 @@ class AnnouncementSerializer(serializers.ModelSerializer):
             'id', 'user', 'images',
             'city', 'district', 'street', 'house_number', 'type',
             'rooms_number', 'floors_number', 'floor_location', 'area',
-            'conditions', 'bathroom', 'kitchen', 'internet', 'intercom',
+            'conditions', 'bathroom', 'kitchen', 'description',
         )
 
-    def get_images(self, obj):
+    def get_images(self, obj): # noqa
         images = AnnouncementImage.objects.filter(announcement=obj)
         return AnnouncementImageSerializer(images, many=True).data
 
@@ -35,16 +49,16 @@ class AnnouncementSerializer(serializers.ModelSerializer):
         return data
 
     def create(self, validated_data):
-        images_data = validated_data.pop('images', [])
+        images_data = validated_data.pop('images')
         announcement = Announcement.objects.create(**validated_data)
         for image_data in images_data:
-            AnnouncementImage.objects.create(announcement=announcement, **image_data)
+            AnnouncementImage.objects.create(announcement=announcement, image=image_data.get('image'))
         return announcement
 
-    # def update(self, instance, validated_data):
-    #     instance.images = validated_data.pop('images', [])
-    #     instance.save()
-    #     announcement = Announcement.objects.get(id=instance.id)
-    #     announcement.update(**validated_data)
-    #     announcement.save()
-    #     return announcement
+    def validate(self, validated_data):
+        floors_number = validated_data.get('floors_number')
+        floor_location = validated_data.get('floor_location')
+
+        if floors_number < floor_location:
+            raise serializers.ValidationError('Floor location must be less than or equal to floors number')
+        return validated_data
